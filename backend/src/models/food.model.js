1e10d99bafba42c34b2164923599dcb7aa720499
Amresh-01 <amreshchaurasiya373@gmail.com>
2025-11-02 23:13:24 +0530

    Add the food models

---
+++ backend/src/models/food.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      max: 5,
+      min: 0,
+      default: 0,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    reviewCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+ffoodSchema.methods.updateRating = function (newRating) {
+  this.averageRating =
+    (this.averageRating * this.reviewCount + newRating) /
+    (this.reviewCount + 1);
+  this.reviewCount += 1;
+  return this.save();
+};
+
+const Food = mongoose.model("Food", foodSchema);
+export default Food;
