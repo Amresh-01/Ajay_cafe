80cd2f5aaeb7c6f47d699de3c91fe4b847c305ad
Amresh-01 <amreshchaurasiya373@gmail.com>
2025-11-08 20:40:04 +0530

    Add fix the role from owner to admin and customer to user

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["owner", "customer"],
+      enum: ["admin", "user"],
       required: true,
       lowercase: true,
     },
