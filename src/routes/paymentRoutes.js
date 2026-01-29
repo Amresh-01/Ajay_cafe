853dc62c46ea1f6d497bb7867d1733b17ab44a88
Amresh-01 <amreshchaurasiya373@gmail.com>
2026-01-29 17:58:50 +0530

    Add protect middleware in payments routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,18 +1,17 @@
 import express from "express";
 import {
-  createOrder,
   verifyPayment,
   getAllPayments,
   getPaymentById,
   createRazorpayOrder,
 } from "../controllers/payment.controller.js";
-import { admin } from "../middlewares/auth.middleware.js";
+import { protect, admin } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/RazorpayOrder", createRazorpayOrder);
-router.post("/verify", verifyPayment);
-router.get("/all", admin, getAllPayments);
-router.get("/:paymentId", getPaymentById);
+router.post("/RazorpayOrder", protect, createRazorpayOrder);
+router.post("/verify", protect, verifyPayment);
+router.get("/all", protect, admin, getAllPayments);
+router.get("/:paymentId", protect, getPaymentById);
 
 export default router;
